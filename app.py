30e2566db91736c55b2344780b631d0931ef298a
rajatrai30 <mailmerairajat@gmail.com>
2022-10-05 18:55:08 +0530

    initial commit 2

--- app.py
+++ app.py
@@ -103,5 +103,17 @@ def video_feed():
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__=='__main__':
     app.run(debug=True)
